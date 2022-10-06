93c7d4c37c271135db2fb6628d9868d8c7e16334
Arlei Nascimento <arleirafael.alvesnascimento@gmail.com>
2022-10-06 09:23:33 -0300

    v.2 javascript functions

---
+++ script.js
@@ -0,0 +1,10 @@
+var menu = document.querySelector('nav ul')
+var menuBar = document.querySelector('nav .menu-icon')
+var iconMenu = document.querySelector('nav')
+var container = document.querySelector('.container')
+
+menuBar.addEventListener('click',function(){
+    menu.classList.toggle('active')
+    
+})
+
