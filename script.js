a15b393b9896d17ada76f030036d4f5123c55372
Arlei Nascimento <arleirafael.alvesnascimento@gmail.com>
2022-10-07 10:08:58 -0300

    v.3 parcialmente finalizada

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 var menu = document.querySelector('nav ul')
 var menuBar = document.querySelector('nav .menu-icon')
 var iconMenu = document.querySelector('nav')
-var container = document.querySelector('.container')
+var menuIcon = document.querySelector('.menu-icon img')
+var button = document.querySelector('ul button')
+
 
 menuBar.addEventListener('click',function(){
     menu.classList.toggle('active')
-    
-})
-
+    menuIcon.setAttribute('src', 'imgs/close.png')
+    button.setAttribute('style', '    background-color: #f1676d;')
+    }
+)
